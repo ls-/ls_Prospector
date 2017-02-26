4bc8b07a92239e4724b5a965813a4fb5a594d91a
Val Voronov <i.lightspark@gmail.com>
2017-02-26 21:18:20 +0700

    Added packager.

---
+++ .packager.ps1
@@ -0,0 +1,56 @@
+Set-Location $PSScriptRoot
+
+if (-Not (Test-Path "C:\PROGRA~1\7-Zip\7z.exe")) {
+	Write-Host "7z.exe not found"
+
+	return Read-Host
+}
+
+Set-Alias 7z "C:\PROGRA~1\7-Zip\7z.exe"
+
+$name = (Get-Item .).Name
+
+if (-Not (Test-Path (".\" + $name + ".toc"))) {
+	Write-Host ".toc not found"
+
+	return Read-Host
+}
+
+if (Get-Content (".\" + $name + ".toc") | Where { $_ -match "(Version: +)([a-zA-Z0-9.-]+)" }) {
+	$version = $matches[2]
+} else {
+	Write-Host "Bad version format"
+
+	return Read-Host
+}
+
+$includedFiles = @(
+	".\LICENSE.txt",
+	".\ls_Prospector.lua",
+	".\ls_Prospector.toc",
+	".\locales\"
+)
+
+$filesToRemove = @(
+	".git",
+	".packager.ps1",
+	"CHANGELOG*",
+	"README*"
+)
+
+if (Test-Path ".\temp\") {
+	Remove-Item ".\temp\" -Recurse -Force
+}
+
+New-Item -Path (".\temp\" + $name) -ItemType Directory | Out-Null
+Copy-Item $includedFiles -Destination (".\temp\" + $name) -Recurse
+Remove-Item ".\temp" -Include $filesToRemove -Recurse -Force
+
+Set-Location ".\temp\"
+
+7z a -tzip -mx9 ($name + "-" + $version + ".zip") (Get-ChildItem -Path "..\temp")
+
+Set-Location "..\"
+
+Move-Item ".\temp\*.zip" -Destination "..\" -Force
+Remove-Item ".\temp" -Recurse -Force
