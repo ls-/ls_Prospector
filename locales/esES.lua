5acac32aa41115270e002490783db5b843941498
Gotzon <gotzon12@gmail.com>
2017-02-27 21:34:36 +0100

    esES Translation (#1)

--- locales/esES.lua
+++ locales/esES.lua
@@ -1,4 +1,4 @@
-﻿-- Contributors:
+﻿-- Contributors: Gotxiko
 
 local _, addonTable = ...
 local L = addonTable.L
@@ -7,3 +7,15 @@ local L = addonTable.L
 local _G = getfenv(0)
 
 if _G.GetLocale() ~= "esES" then return end
+ 
+L["LS_PROSPECTOR"] = "ls: |cff1a9fc0Prospector|r"
+
+-- Require translation
+L["CREATE_MACRO"] = "Crear macro"
+L["COLOR_BLUE"] = "Azul"
+L["COLOR_GREEN"] = "Verde"
+L["COLOR_ORANGE"] = "Naranja"
+L["COLOR_PURPLE"] = "Morada"
+L["COLOR_RED"] = "Roja"
+L["COLOR_YELLOW"] = "Amarilla"
+L["SWAP_ORE"] = "Cambiar mena"
